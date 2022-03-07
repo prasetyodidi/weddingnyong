ed92960a2ce0d120c508ab61e2cc3ed7caae6310
Didi Prasetyo <didiprasetyo360@gmail.com>
2022-03-07 16:22:24 +0700

    rsvp email guest book confirmation

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AttendeeListController;
 use Zxing\QrReader;
 use App\Models\Design;
 use Illuminate\Support\Facades\URL;
@@ -10,6 +11,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DesignController;
 use App\Http\Controllers\GuestBookController;
 use App\Http\Controllers\InvitationController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,38 +27,30 @@ use App\Http\Controllers\InvitationController;
 Route::get('/', function () {
     $designs = Design::limit(6)->get();
     return view('home', compact('designs'));
-});
+})->name('landing.page');
 Route::get('/qr', function () {
     $image = URL::to('/') . '/img/invitation/qr/77QR.svg';
     $qrcode = new QrReader($image);
     $text = $qrcode->text();
     echo $text;
 });
-Route::get('/qr/create', [QrController::class, 'create']);
-
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::get('/dashboard/invitation', [DashboardController::class, 'invitations'])->name('dashboard.invitation');
-    Route::get('/dashboard/guest-book', [DashboardController::class, 'guestBooks']);
-    Route::get('/dashboard/attendee-list', [DashboardController::class, 'attendeeList']);
+    Route::get('/dashboard/guest-book', [DashboardController::class, 'guestBooks'])->name('dashboard.guest_book');
+    Route::get('/dashboard/attendee-list', [DashboardController::class, 'attendeeList'])->name('dashboard.attendee_list');
     Route::get('/dashboard/user', [DashboardController::class, 'users']);
     Route::get('/dashboard/design', [DashboardController::class, 'design']);
+    Route::get('/dashboard/profile', [DashboardController::class, 'profile'])->name('dashboard.profile');
+    route::resource('/design', DesignController::class)->parameters([
+        'design' => 'design:slug'
+    ]);
+    Route::put('/user/{user}', [UserController::class, 'update'])->name('user.update');
 });
-
-Route::get('/demo/winter', function () {
-    return Storage::path('file.jpg');
-});
-
-Route::get('/test/{design}', function (string $design) {
-    return view('designs/' . $design . '/demo_design');
-});
-
 Route::resource('/invitation', InvitationController::class)->parameters([
     'invitation' => 'invitation:slug'
 ]);
-
 Route::post('/guest-book/create', [GuestBookController::class, 'store'])->name('guest-book.post');
 Route::post('/guest-book/index', [GuestBookController::class, 'index'])->name('guest-book.index');
-Route::get('/design', [DesignController::class, 'index'])->name('design.all');
 
 require __DIR__ . '/auth.php';
