26face8f71efc1b8d80c8039ee0771ccd3315fb5
didi <didiprasetyo360@gmail.com>
2022-11-11 14:06:54 +0700

    update how to get embed maps url

--- app/Http/Controllers/InvitationController.php
+++ app/Http/Controllers/InvitationController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Helpers\Helper;
 use App\Models\Invitation;
 use App\Models\Design;
-use App\Http\Requests\UpdateInvitationRequest;
 use Cviebrock\EloquentSluggable\Services\SlugService;
-use Illuminate\Http\Request as HttpRequest;
-
-require('/laragon/www/online-invitation/app/Models/simple_html_dom.php');
-// require('App/Models/simple_html_dom.php');
+use Illuminate\Http\Request;
 
 class InvitationController extends Controller
 {
+    private $fileRules = 'required|image|file|max:2048';
+    private $textRules = 'required|max:255';
+    private $timeRules = 'required|date_format:H:i';
+    private $dateRules = 'required|date';
+    private $imageFolder = 'img/invitation';
+
     public function __construct()
     {
         $this->middleware(['verified', 'auth'])->except(['show', 'create']);
@@ -21,25 +23,12 @@ class InvitationController extends Controller
         // ->only([]);
         // ->except([]);
     }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // show all invitation
-    // only admin
+
     public function index()
     {
         return 'default invitation/read invitation';
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    // user
-    // must verify, auth
     public function create()
     {
         $slug = null;
@@ -47,7 +36,6 @@ class InvitationController extends Controller
             $slug = $_GET['slug'];
         }
         $designs = Design::all();
-        $design = null;
         if (isset($slug)) {
             $design = Design::where('slug', $slug)->get()->first();
             return view('/invitation/create', compact('designs', 'design'));
@@ -55,36 +43,28 @@ class InvitationController extends Controller
         return view('/invitation/create', compact('designs'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreInvitationRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    // user
-    // must verify, auth
-    public function store(HttpRequest $request)
+    public function store(Request $request)
     {
         $rules = [
-            'cover_image' => 'required|image|file|max:2048',
-            'groom_image' => 'required|image|file|max:2048',
-            'bride_image' => 'required|image|file|max:2048',
-            'groom_name' => 'required|max:255',
-            'groom_fullname' => 'required|max:255',
+            'cover_image' => $this->fileRules,
+            'groom_image' => $this->fileRules,
+            'bride_image' => $this->fileRules,
+            'groom_name' => $this->textRules,
+            'groom_fullname' => $this->textRules,
             'groom_info' => 'required',
-            'bride_name' => 'required|max:255',
-            'bride_fullname' => 'required|max:255',
+            'bride_name' => $this->textRules,
+            'bride_fullname' => $this->textRules,
             'bride_info' => 'required',
-            'wedding_date' => 'required|date',
-            'wedding_time_start' => 'required|date_format:H:i',
-            'wedding_time_end' => 'required|date_format:H:i',
-            'wedding_address' => 'required|max:255',
-            'wedding_venue' => 'required|max:255',
-            'reception_date' => 'required|date',
-            'reception_time_start' => 'required|date_format:H:i',
-            'reception_time_end' => 'required|date_format:H:i',
-            'reception_address' => 'required|max:255',
-            'reception_venue' => 'required|max:255',
+            'wedding_date' => $this->dateRules,
+            'wedding_time_start' => $this->timeRules,
+            'wedding_time_end' => $this->timeRules,
+            'wedding_address' => $this->textRules,
+            'wedding_venue' => $this->textRules,
+            'reception_date' => $this->dateRules,
+            'reception_time_start' => $this->timeRules,
+            'reception_time_end' => $this->timeRules,
+            'reception_address' => $this->textRules,
+            'reception_venue' => $this->textRules,
             'embed_maps' => 'required',
             'design_id' => 'required',
             // 'price_id' => 'required',
@@ -99,11 +79,23 @@ class InvitationController extends Controller
         ]);
         $validateData['audio'] = 'wedding audio1.mp3';
 
-        $validateData['cover_image'] = $request->file('cover_image')->store('img/invitation', ['disk' => 'directly_to_public']);
-        $validateData['groom_image'] = $request->file('groom_image')->store('img/invitation', ['disk' => 'directly_to_public']);
-        $validateData['bride_image'] = $request->file('bride_image')->store('img/invitation', ['disk' => 'directly_to_public']);
+        $validateData['cover_image'] = $request->file('cover_image')
+            ->store(
+                $this->imageFolder,
+                ['disk' => 'directly_to_public']
+            );
+        $validateData['groom_image'] = $request->file('groom_image')
+            ->store(
+                $this->imageFolder,
+                ['disk' => 'directly_to_public']
+            );
+        $validateData['bride_image'] = $request->file('bride_image')
+            ->store(
+                $this->imageFolder,
+                ['disk' => 'directly_to_public']
+            );
 
-        $validateData['user_id'] = auth()->user()->id;
+        $validateData['user_id'] = $request->user()->id;
         $validateData['price_id'] = 1;
 
         Invitation::create($validateData);
@@ -111,29 +103,17 @@ class InvitationController extends Controller
         return redirect('/dashboard/invitation')->with('message', 'New invitation has been created');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Invitation  $invitation
-     * @return \Illuminate\Http\Response
-     */
     // guest
     public function show(Invitation $invitation)
     {
-        $invitation_id = $invitation->id;
         $designSlug = Design::where('id', $invitation->design_id)->get()->first()->slug;
-        $html = str_get_html($invitation->embed_maps);
-        $invitation->embed_maps = $html->find('iframe')[0]->src;
+        $result = [];
+        preg_match('/src="([^"]+)"/', $invitation->embed_maps, $result);
+        $invitation->embed_maps = $result[1];
 
         return view('designs/' . $designSlug . '/code_design', compact('invitation'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Invitation  $invitation
-     * @return \Illuminate\Http\Response
-     */
     // only invitee owner & admin
     public function edit(Invitation $invitation)
     {
@@ -142,37 +122,30 @@ class InvitationController extends Controller
         return view('invitation/edit', compact('invitation', 'designs'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateInvitationRequest  $request
-     * @param  \App\Models\Invitation  $invitation
-     * @return \Illuminate\Http\Response
-     */
     // only invitee owner & admin
-    public function update(HttpRequest $request, Invitation $invitation)
+    public function update(Request $request, Invitation $invitation)
     {
         $this->checkOwnerOrAdmin($invitation);
         $rules = [
-            'cover_image' => 'image|file|max:2048',
-            'groom_image' => 'image|file|max:2048',
-            'bride_image' => 'image|file|max:2048',
-            'groom_name' => 'required|max:255',
-            'groom_fullname' => 'required|max:255',
+            'cover_image' => $this->fileRules,
+            'groom_image' => $this->fileRules,
+            'bride_image' => $this->fileRules,
+            'groom_name' => $this->textRules,
+            'groom_fullname' => $this->textRules,
             'groom_info' => 'required',
-            'bride_name' => 'required|max:255',
-            'bride_fullname' => 'required|max:255',
+            'bride_name' => $this->textRules,
+            'bride_fullname' => $this->textRules,
             'bride_info' => 'required',
-            'wedding_date' => 'required|date',
-            'wedding_time_start' => 'required|date_format:H:i',
-            'wedding_time_end' => 'required|date_format:H:i',
-            'wedding_address' => 'required|max:255',
-            'wedding_venue' => 'required|max:255',
-            'reception_date' => 'required|date',
-            'reception_time_start' => 'required|date_format:H:i',
-            'reception_time_end' => 'required|date_format:H:i',
-            'reception_address' => 'required|max:255',
-            'reception_venue' => 'required|max:255',
+            'wedding_date' => $this->dateRules,
+            'wedding_time_start' => $this->timeRules,
+            'wedding_time_end' => $this->timeRules,
+            'wedding_address' => $this->textRules,
+            'wedding_venue' => $this->textRules,
+            'reception_date' => $this->dateRules,
+            'reception_time_start' => $this->timeRules,
+            'reception_time_end' => $this->timeRules,
+            'reception_address' => $this->textRules,
+            'reception_venue' => $this->textRules,
             'embed_maps' => 'required',
             'design_id' => 'required',
             // 'price_id' => 'required',
@@ -182,25 +155,31 @@ class InvitationController extends Controller
         $validateData = $request->validate($rules);
 
         if ($request->cover_image) {
-            $validateData['cover_image'] = $request->file('cover_image')->store('img/invitation', ['disk' => 'directly_to_public']);
+            $validateData['cover_image'] = $request->file('cover_image')
+                ->store(
+                    $this->imageFolder,
+                    ['disk' => 'directly_to_public']
+                );
         }
         if ($request->bride_image) {
-            $validateData['bride_image'] = $request->file('bride_image')->store('img/invitation', ['disk' => 'directly_to_public']);
+            $validateData['bride_image'] = $request->file('bride_image')
+                ->store(
+                    $this->imageFolder,
+                    ['disk' => 'directly_to_public']
+                );
         }
         if ($request->groom_image) {
-            $validateData['groom_image'] = $request->file('groom_image')->store('img/invitation', ['disk' => 'directly_to_public']);
+            $validateData['groom_image'] = $request->file('groom_image')
+                ->store(
+                    $this->imageFolder,
+                    ['disk' => 'directly_to_public']
+                );
         }
         $invitation->update($validateData);
 
         return 'upload updated data to database';
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Invitation  $invitation
-     * @return \Illuminate\Http\Response
-     */
     // only invitee owner & admin
     public function destroy(Invitation $invitation)
     {
@@ -212,7 +191,6 @@ class InvitationController extends Controller
     public function checkSlug($names)
     {
         $name = $names['groom_name'] . '-' . $names['bride_name'];
-        $slug = SlugService::createSlug(Invitation::class, 'slug', $name);
-        return $slug;
+        return SlugService::createSlug(Invitation::class, 'slug', $name);
     }
 }
